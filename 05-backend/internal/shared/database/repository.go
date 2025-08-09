aff03cfeca915cbd7fa188d6ec84d6b871a5e800
shvdg <stanhvdg@gmail.com>
2025-08-09 17:56:46 +0200

    Replace `sql/queries.go` with `database/repository.go`: Introduce `Repository` interface for consistent CRUD operations and remove redundant SQL query generator.

---
+++ 05-backend/internal/shared/database/repository.go
@@ -0,0 +1,19 @@
+package database
+
+import "context"
+
+// Repository defines the interface for database repositories.
+type Repository[T any] interface {
+	CreateOne(context.Context, T) error
+	ReadOne(context.Context, string, []string, ScannerFunc[T]) (T, error)
+	UpdateOne(context.Context, T) error
+	DeleteOne(context.Context, T) error
+
+	CreateMany(context.Context, []T) error
+	ReadMany(context.Context, []string, ScannerFunc[T]) (T, error)
+	UpdateMany(context.Context, []T) error
+	DeleteMany(context.Context, []T) error
+
+	CreateTable(context.Context) error
+	DropTable(context.Context) error
+}
--- 05-backend/internal/shared/sql/queries.go
+++
@@ -1,8 +0,0 @@
-package sql
-
-import "fmt"
-
-// CreateDeleteQuery creates a SQL delete statement
-func CreateDeleteQuery(tableName, where string) string {
-	return fmt.Sprintf("DELETE FROM %s WHERE %s", tableName, where)
-}
