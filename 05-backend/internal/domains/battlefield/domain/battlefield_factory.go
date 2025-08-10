88a67390d5d4645f764b6aa0a9b7042e1f4e0900
shvdg <stanhvdg@gmail.com>
2025-08-10 19:11:38 +0200

    Remove `battlefield` domain factory method and introduce protobuf definitions for `characterFormation` and `formation` domains, updating `characterUnit` package path for consistency.

--- 05-backend/internal/domains/battlefield/domain/battlefield_factory.go
+++
@@ -1,6 +0,0 @@
-package domain
-
-// CreateDefaultBattlefield creates a new default battlefield.
-func CreateDefaultBattlefield() *Battlefield {
-	return NewBattlefieldBuilder().AddEmptyRows(1, 10).Build()
-}
