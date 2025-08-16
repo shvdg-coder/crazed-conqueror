6cb053cc250e77e3bf30a40a2edff8f3bfe158d3
shvdg <stanhvdg@gmail.com>
2025-08-16 17:29:00 +0200

    Remove Battlefield domain and related components.

--- 05-backend/internal/domains/battlefield/domain/battlefield_column.go
+++
@@ -1,9 +0,0 @@
-package domain
-
-import "shvdg/crazed-conquerer/internal/shared/types"
-
-// BattlefieldColumn represents a single column in the battlefield.
-type BattlefieldColumn struct {
-	Coordinates *types.Coordinates
-	OccupantIds []string
-}
--- 05-backend/internal/domains/battlefield/domain/battlefield_row.go
+++
@@ -1,6 +0,0 @@
-package domain
-
-// BattlefieldRow represents a single row in the battlefield.
-type BattlefieldRow struct {
-	Columns []BattlefieldColumn
-}
